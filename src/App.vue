df412c9c3d64b1567fd4ec930cbb6c15491b65dd
ChalalSamir <samir-94@outlook.com>
2020-05-19 15:38:29 +0200

    footer

--- src/App.vue
+++ src/App.vue
@@ -8,16 +8,20 @@
     </div>
     -->
     <router-view/>
+    <Footer />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
+
 
 export default {
   components: {
-    Header
+    Header,
+    Footer
   }
 }
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,6 @@
+<template>
+  <footer class="footer">
+    <h3>On t'aime booba</h3>
+  </footer>
+</template>
+
