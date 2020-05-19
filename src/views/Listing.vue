a2b5559f44e79e88fbc9f54e9cbad386222f6570
ChalalSamir <samir-94@outlook.com>
2020-05-19 19:00:51 +0200

    updating waypoint

--- src/views/Listing.vue
+++ src/views/Listing.vue
@@ -2,7 +2,7 @@
   <div class="listing">
     <h2>ACTUS</h2>
     <div class="listing__content">
-      <div v-waypoint="{ active: true, callback: onWaypoint , options: { threshold: [1 , 1] }}" class="listing__article" v-for="post in posts" :key="post.id">
+      <div v-waypoint="{ active: true, callback: onWaypoint , options: intersectionOptions}" class="listing__article listing__page" v-for="post in posts" :key="post.id">
         <h4>{{ post.title }}</h4>
         <div>
           <router-link :to="`/article/${ post.id }`">
@@ -18,7 +18,12 @@
 export default {
   data () {
     return {
-      posts: null
+      posts: null,
+      intersectionOptions: {
+        root: null,
+        rootMargin: '0px 0px 0px 0px',
+        threshold: [.7, 1] // [0.25, 0.75] if you want a 25% offset!
+      } 
     }
     
   },
