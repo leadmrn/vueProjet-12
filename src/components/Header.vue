874d2c90fe2258ef38b953cf3e379f7968088d78
Remi Cakir <remi.cakir@hxperience.com>
2020-05-19 18:12:35 +0200

    add dark mode

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="header">
     <h3 class="header__logo"><router-link to="/">Boobagarre</router-link></h3>
+    <div class="theme-switch-wrapper">
+      <label class="theme-switch" for="checkbox">
+          <input v-model="theme" type="checkbox" id="checkbox" />
+          <div class="slider round"></div>
+      </label>
+    </div>
     <div class=header__icon>
       <div></div>
       <div></div>
@@ -8,4 +14,22 @@
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      theme: null
+    }
+  },
+  watch: {
+    theme: function(e) {
+      console.log(e);
+      e ? document.documentElement.setAttribute('data-theme', 'dark'): document.documentElement.setAttribute('data-theme', 'light');
+    }
+  }
+}
+</script>
+
+
 
