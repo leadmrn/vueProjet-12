be2881d6be1709fab70eb52e7ee68b6c915a5e12
Léa De Amorin <leadeamorin.ps@gmail.com>
2020-05-20 14:06:19 +0200

    favicon

--- vue.config.js
+++ vue.config.js
@@ -8,6 +8,13 @@ module.exports = {
 		manifestOptions: {
 			display: 'fullscreen',
 		},
+		iconPaths: {
+			favicon32: 'img/icons/favicon-32x32.png',
+			favicon16: 'img/icons/favicon-16x16.png',
+			appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+			maskIcon: 'img/icons/safari-pinned-tab.svg',
+			msTileImage: 'img/icons/msapplication-icon-144x144.png'
+		},
 		workboxPluginMode: 'InjectManifest',
 		workboxOptions: {
 			swSrc: 'src/service-worker.js'
